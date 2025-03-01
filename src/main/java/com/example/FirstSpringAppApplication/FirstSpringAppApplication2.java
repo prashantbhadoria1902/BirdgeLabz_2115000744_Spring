72a57983218c0938c4af7371a9a93fae092b6f61
prashantbhadoria1902 <prashantbhadoria1902@gmail.com>
2025-03-01 16:31:59 +0530

    [Prashant] Updated UC2

--- src/main/java/com/example/FirstSpringAppApplication/FirstSpringAppApplication2.java
+++ src/main/java/com/example/FirstSpringAppApplication/FirstSpringAppApplication2.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FirstSpringAppApplication2 {
     public static void main(String[] args) {
-        SpringApplication.run(FirstSpringAppApplication.class, args);
+        SpringApplication.run(FirstSpringAppApplication2.class, args);
     }
 }
